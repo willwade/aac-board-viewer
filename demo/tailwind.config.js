c8e528f177efaebf5249a07ca23d8ba75eef6be2
Will Wade <willwade@gmail.com>
2026-01-06 13:54:43 +0000

    Update tailwind.config.js

--- demo/tailwind.config.js
+++ demo/tailwind.config.js
@@ -2,7 +2,8 @@
 export default {
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
-    "./dist/**/*.{js,jsx,ts,tsx}"
+    "./dist/**/*.{js,jsx,ts,tsx}",
+    "./node_modules/aac-board-viewer/**/*.{js,mjs,cjs,ts,tsx}"
   ],
   theme: {
     extend: {},
